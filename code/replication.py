ce78ceaedaa516f9266e22684f93756937d80d53
Shane McAndrew <55311029+shanemcandrewai@users.noreply.github.com>
2020-05-13 18:38:23 +0200

    refact, obpursed, 20161217

--- code/replication.py
+++ code/replication.py
@@ -13,29 +13,27 @@ import dlib
 
 class Frames:
     """ Frame file manager """
-    def __init__(self, dirs=None, video=None, suffix='.jpeg', num_len=4):
-        if dirs is None:
-            self.dirs = {'frames' : Path('..', 'replic', 'frames'),
-                         'data' : Path('..', 'replic', 'data')}
+    def __init__(self, frames_dir=None, video=None, suffix='.jpeg', num_len=4):
+        if frames_dir is None:
+            self.frames_dir = Path('..', 'replic', 'frames')
         else:
-            self.dirs = dirs
+            self.frames_dir = Path(frames_dir)
         if video is None:
             self.video = Path('..', 'replic', 'samples', 'obama2s.mp4')
         else:
             self.video = Path(video)
-        self.dirs['frames'].mkdir(parents=True, exist_ok=True)
-        self.dirs['data'].mkdir(parents=True, exist_ok=True)
+        self.frames_dir.mkdir(parents=True, exist_ok=True)
         self.suffix = suffix
         self.num_len = num_len
 
     def get_file_path(self, frame_num=30):
         """ Build file path from frame number """
-        return Path(self.dirs['frames'], str(frame_num).zfill(
+        return Path(self.frames_dir, str(frame_num).zfill(
             self.num_len)).with_suffix(self.suffix)
 
     def get_frame_file_names(self):
         """ Get list of frame files """
-        return sorted(self.dirs['frames'].glob('*' + self.suffix))
+        return sorted(self.frames_dir.glob('*' + self.suffix))
 
     def get_frame_nums(self):
         """ Get list of frame numbers """
@@ -110,7 +108,11 @@ class DlibProcess:
 
 class DataProcess:
     """ Calculations and supporting methods required for the replication of experiments """
-    def __init__(self, frames=None):
+    def __init__(self, data_dir=None, frames=None):
+        if data_dir is None:
+            self.data_dir = Path('..', 'replic', 'data')
+        else:
+            self.data_dir = data_dir
         if frames is None:
             self.frames = Frames()
         else:
@@ -126,14 +128,14 @@ class DataProcess:
             dlib_proc = DlibProcess()
         if new_extract:
             self.all_lmarks = None
-        elif Path(self.frames.dirs['data'], extract_file).is_file():
-            self.all_lmarks = np.load(Path(self.frames.dirs['data'], extract_file))
+        elif Path(self.data_dir, extract_file).is_file():
+            self.all_lmarks = np.load(Path(self.data_dir, extract_file))
         if self.all_lmarks.size == 0:
             if not self.frames.get_frame_nums():
                 Video().extract_frames(extract_file.with_suffix('.mp4'))
             for frame_num in self.frames.get_frame_nums():
                 self.all_lmarks = np.concatenate([self.all_lmarks, dlib_proc.get_lmarks(frame_num)])
-            np.save(Path(self.frames['data'], extract_file), self.all_lmarks)
+            np.save(Path(self.data_dir, extract_file), self.all_lmarks)
         return self.all_lmarks
 
     def get_procrustes(self, extract_file='obama2s.npy', lips_only=False):
@@ -341,7 +343,11 @@ class Draw:
 
 class Video:
     """ FFmpeg video processing manager """
-    def __init__(self, audio_dir=None, frames=None):
+    def __init__(self, video_dir=None, audio_dir=None, frames=None):
+        if video_dir is None:
+            self.video_dir = Path('..', 'replic', 'video')
+        else:
+            self.video_dir = Path(video_dir)
         if audio_dir is None:
             self.audio_dir = Path('..', 'replic', 'audio')
         else:
@@ -362,11 +368,11 @@ class Video:
 
     def extract_frames(self, video_in='obama2s.mp4', start_number=0, quality=5):
         """ Extract frames from video using FFmpeg """
-        frame_dir = Path(self.frames.frame_dir)
+        frame_dir = Path(self.frames.frames_dir)
         if frame_dir.is_dir():
             shutil.rmtree(frame_dir)
         frame_dir.mkdir(parents=True, exist_ok=True)
-        sp.run(['ffmpeg', '-i', str(Path(self.frames.dirs['video'], video_in)),
+        sp.run(['ffmpeg', '-i', str(Path(self.video_dir, video_in)),
                 '-start_number', str(start_number), '-qscale:v', str(quality),
                 str(Path(frame_dir, r'%0' + str(
                     self.frames.num_len) + 'd' + self.frames.suffix))], check=True)
